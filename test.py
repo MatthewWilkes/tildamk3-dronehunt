66b542e5b357570efaeb9f05c57106ad366c3b1f
Matthew Wilkes <git@matthewwilkes.name>
2016-08-09 23:54:18 +0100

    Minor polish, HABs

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+### Author: Matthew Wilkes
+### Description: Shoot down unauthorised flying toys
+### Category: Games 
+### License: MIT
+### Appname : dronehunt
+
 import buttons
 import ugfx
 import micropython
@@ -70,8 +76,8 @@ def redraw_whole_bg():
 def redraw_bg_range(x: int, y: int, to_x: int, to_y: int):
     if x < 0: x = 0
     if y < 0: y = 0
-    if x > 319: x = 319
-    if y > 239: y = 239
+    if to_x > 319: to_x = 319
+    if to_y > 239: to_y = 239
 
     if to_y < 190:
         # This is just sky
@@ -122,12 +128,15 @@ def move_crosshair(move_x, move_y):
 
     draw_crosshair(crosshair_x, crosshair_y)
     
+def refresh_score():
+    ugfx.set_default_font(ugfx.FONT_SMALL)
+    ugfx.text(10, 10, "%d" % score, ugfx.WHITE)
+
 def award_points(points):
     global score
     score += points
-    ugfx.set_default_font(ugfx.FONT_SMALL)
     redraw_bg_range(10, 10, 20, 100)
-    ugfx.text(10, 10, "%d" % score, ugfx.WHITE)
+    refresh_score()
 
 
 def is_hit(x_aim, y_aim):
@@ -151,6 +160,9 @@ def shoot():
 bz=pyb.Pin(pyb.Pin.cpu.D12, pyb.Pin.OUT_PP)
 
 def tone(f,t,b=0):
+    f = int(f)
+    t = int(t)
+    b = int(b)
     global bz
     p = int(1000000/f)
     t1 = pyb.millis()+t
@@ -223,11 +235,45 @@ class Quadcopter(object):
             elif self.animation_frame == 1:
                 ugfx.line(rotor_center_x, rotor_center_y - QUADCOPTER_BODY_SIZE, rotor_center_x, rotor_center_y + QUADCOPTER_BODY_SIZE, ugfx.BLACK)
             elif self.animation_frame == 2:
-                ugfx.line(rotor_center_x + int(QUADCOPTER_BODY_SIZE*0.7), rotor_center_y + int(QUADCOPTER_BODY_SIZE*0.7), rotor_center_x - int(QUADCOPTER_BODY_SIZE*0.7), rotor_center_y - int(QUADCOPTER_BODY_SIZE*0.7), ugfx.BLACK)
+                ugfx.line(rotor_center_x + int(QUADCOPTER_BODY_SIZE*0.7), rotor_center_y - int(QUADCOPTER_BODY_SIZE*0.7), rotor_center_x - int(QUADCOPTER_BODY_SIZE*0.7), rotor_center_y + int(QUADCOPTER_BODY_SIZE*0.7), ugfx.BLACK)
             elif self.animation_frame == 3:
-                ugfx.line(rotor_center_x - QUADCOPTER_BODY_SIZE, rotor_center_y, rotor_center_x + QUADCOPTER_BODY_SIZE, rotor_center_y, ugfx.BLACK)
+                ugfx.line(rotor_center_x + QUADCOPTER_BODY_SIZE, rotor_center_y, rotor_center_x - QUADCOPTER_BODY_SIZE, rotor_center_y, ugfx.BLACK)
+        draw_crosshair(crosshair_x, crosshair_y)
+
+class HAB(object):
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.color = self.color = random_choice((ugfx.BLACK, ugfx.ORANGE, ugfx.GREEN, ugfx.YELLOW, ugfx.PURPLE))
+    
+    @property
+    def score(self):
+        return 20
+    
+    def move_copter(self):
+        if self.y < -20:
+            raise ValueError("Escaped!")
+        self.undraw()
+        self.y -= 4
+        self.draw()
+    
+    def undraw(self):
+        redraw_bg_range(
+            self.x - (QUADCOPTER_BODY_SIZE*5),
+            self.y - (QUADCOPTER_BODY_SIZE*5),
+            self.x + (QUADCOPTER_BODY_SIZE*5),
+            self.y + (QUADCOPTER_BODY_SIZE*5)
+        )
+    
+    def draw(self):
+        ugfx.fill_circle(self.x, self.y, QUADCOPTER_BODY_SIZE*2, ugfx.GRAY)
+        ugfx.line(self.x, self.y+(QUADCOPTER_BODY_SIZE), self.x, self.y+QUADCOPTER_BODY_SIZE*3, ugfx.BLACK)
+        ugfx.area(self.x-5, self.y+QUADCOPTER_BODY_SIZE*3, QUADCOPTER_BODY_SIZE*2, QUADCOPTER_BODY_SIZE, self.color)
         draw_crosshair(crosshair_x, crosshair_y)
     
+
+
 def spawn_quadcopter():
     if len(quadcopters) > level:
         print("Too many copters! Call the CAA")
@@ -241,9 +287,29 @@ def spawn_quadcopter():
     else:
         x = 320
         direction = '-'
-    speed = (pyb.rng() % (level-1)) + 1
+    speed = (pyb.rng() % (level))
+    if speed == 0:
+        speed = 1
     quadcopter = Quadcopter(x, pyb.rng() % 160, direction=direction, speed=speed)
     quadcopters.append(quadcopter)
+
+def spawn_hab():
+    if len(quadcopters) > level:
+        print("Too many copters! Call the CAA")
+        #global tmc_count
+        #tmc_count += 1
+        return
+    quadcopter = HAB(pyb.rng()%300, 180, speed=1)
+    quadcopters.append(quadcopter)
+
+def spawn_enemy():
+    if level == 1:
+        spawn_quadcopter()
+    else:
+        if pyb.rng() % 5 == 0:
+            spawn_hab()
+        else:
+            spawn_quadcopter()
 
 def animate_quadcopters():
     for quadcopter in quadcopters:
@@ -258,6 +324,10 @@ frame_times = []
 def die():
     global lives
     lives -= 1
+    tone(300,100,30)
+    tone(200,100,30)
+    tone(100,100,30)
+    tone(50,200,30)
 
 def draw_fps():
     ugfx.set_default_font(ugfx.FONT_SMALL)
@@ -286,8 +356,51 @@ def maybe_advance_level():
     if new_level != level:
         #notice(text="You have reached level %d" % level, title="Congratulations")
         level = new_level
+        tone(100,250,30)
+        tone(150,50,30)
+        tone(180,50,30)
+        tone(150,50,30)
+        tone(180,50,30)
+        tone(150,50,30)
+        tone(180,50,30)
+        tone(100,250,30)
         redraw_whole_bg()
-        
+        for copter in quadcopters:
+            copter.draw()
+
+
+A = 880
+AS = 932.33
+C = 1046.50
+CS = 1108.73
+D = 1174.66
+SPEED = 500
+
+ugfx.clear(SKIES[level])
+ugfx.set_default_font(ugfx.FONT_NAME)
+ugfx.text(110, 10, "EMF", ugfx.WHITE)
+ugfx.set_default_font(ugfx.FONT_MEDIUM)
+ugfx.text(100, 80, "It's unbelievable!", ugfx.WHITE)
+
+ugfx.text(10, 140, "There are too many toys in the skies.", ugfx.WHITE)
+ugfx.text(95, 160, "Shoot them down.", ugfx.WHITE)
+
+
+tone(A, SPEED*0.75, SPEED*0.25)
+tone(A, SPEED*0.5, SPEED*0.5)
+tone(C, SPEED*0.2, SPEED*0.1)
+tone(A, SPEED*0.3, SPEED*0.3)
+tone(C, SPEED*0.4, SPEED*0.25)
+tone(C, SPEED*0.5, SPEED*0.4)
+tone(CS, SPEED*0.4, SPEED*0.25)
+tone(D, SPEED*0.15, SPEED*0.4)
+tone(D, SPEED*0.6, SPEED*0.4)
+tone(C, SPEED*0.5, SPEED*0.4)
+tone(C, SPEED*0.3, 0)
+tone(AS, SPEED*0.2, SPEED*0.3)
+tone(AS, SPEED*0.4, SPEED*0.1)
+tone(A, SPEED*0.1, SPEED*0.4)
+tone(A, SPEED*0.6, SPEED*0.4)
 
 playing = True
 while playing:
@@ -307,18 +420,18 @@ while playing:
             move_crosshair(0, -5)
         if buttons.is_pressed("JOY_DOWN"):
             move_crosshair(0, 5)
-        if buttons.is_pressed("JOY_CENTER"):
+        if buttons.is_pressed("JOY_CENTER") or buttons.is_pressed("BTN_A") or buttons.is_pressed("BTN_B"):
             shoot()
         if pyb.millis() > next_change:
             next_change = pyb.millis() + SCREEN_DURATION
             frame_times.append(pyb.millis())
             frame_times = frame_times[-50:]
-            draw_fps()
-            print(pixels)
-            pixels = 0
+            #print(pixels)
+            #pixels = 0
             animate_quadcopters()
         if pyb.millis() > next_enemy:
-            spawn_quadcopter()
+            spawn_enemy()
             next_enemy = pyb.millis () + ENEMY_FREQUENCY - (level * 300)
         maybe_advance_level()
+        refresh_score()
     playing = prompt_boolean("You scored %d. Try again?" % score, title="Game over!")
